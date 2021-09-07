d2ae4812a5c2aadcaaff02abc25f38a9bd805deb
Joseph Marie Alba <josephalbaph@gmail.com>
2021-09-08 03:48:45 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,14 +45,7 @@ const Home: FC = () => (
       </div>
     </main>
 
-    <footer className={styles.footer}>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by Joseph Alba
-      </a>
+    <footer className={styles.footer}>Powered by Joseph Alba</a>
     </footer>
   </div>
 );
